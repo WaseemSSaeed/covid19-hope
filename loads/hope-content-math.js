203f8df063834e9d2305c376166b4ab56d10d154
Waseem Saeed <57914762+WaseemSSaeed@users.noreply.github.com>
2020-03-18 23:01:43 +0200

    Add files via upload

---
+++ loads/hope-content-math.js
@@ -0,0 +1,4 @@
+document.writeln("\
+While typing this, COVID-19 worldwide reached 215000+ cases, it's about 0.0027% of world population.\
+              virus spreading is kind of always starting with a logarithmic curve wich means a lot of accelartion\
+              <a href='#math' style='color: royalblue;'>(More <span style='font-family: leCafeTech2; font-size: 16px'>f</span> )</a>");

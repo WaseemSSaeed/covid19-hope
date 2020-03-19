59786d822c16cfac1e3f040a6e13dc00cef73313
Waseem Saeed <57914762+WaseemSSaeed@users.noreply.github.com>
2020-03-19 18:30:57 +0200

    Add files via upload

---
+++ loads/news/newsData.js
@@ -0,0 +1,14 @@
+
+var nData = [["UK","BBC","When your children's school is closed,\
+                or if you have a relative who is ill, your employer must give you time off to look after them.",
+              "https://www.bbc.com/news/business-51628524"],
+
+              ["Global","CNN","What you need to know about coronavirus on Thursday, March 19",
+              "https://edition.cnn.com/2020/03/19/world/coronavirus-newsletter-03-19-20/index.html"],
+
+              ["Global","WHO","Country & Technical Guidance - Coronavirus disease (COVID-19)",
+              "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/technical-guidance","star"],
+
+                ["Italy","France24","Italy prolongs coronavirus lockdown measures to April 3",
+                "https://www.france24.com/en/20200319-italy-to-prolong-coronavirus-lockdown-measures"]
+              ];

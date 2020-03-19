c8c64981341d175a4fa085a3a2dd478933681633
Waseem Saeed <57914762+WaseemSSaeed@users.noreply.github.com>
2020-03-19 22:53:55 +0200

    Add files via upload

--- loads/news/newsData.js
+++ loads/news/newsData.js
@@ -3,12 +3,20 @@ var nData = [["UK","BBC","When your children's school is closed,\
                 or if you have a relative who is ill, your employer must give you time off to look after them.",
               "https://www.bbc.com/news/business-51628524"],
 
+              ["USA | Global","Education","Cambridge University Opens FREE ACCESS to scholarly content published by Cambridge University Press \
+              Cambridge University Press publishes a wide range of high-quality academic content across\
+              Cambridge Core, including leading journals, research monographs, reference works and textbooks.\
+              ", "https://www.cambridge.org/core/what-we-publish", "edu"],
+
               ["Global","CNN","What you need to know about coronavirus on Thursday, March 19",
               "https://edition.cnn.com/2020/03/19/world/coronavirus-newsletter-03-19-20/index.html"],
+
+              ["Global","Education","Coursera offers free access to universities closed by coronavirus",
+              "https://www.coursera.org/coronavirus","edu"],
 
               ["Global","WHO","Country & Technical Guidance - Coronavirus disease (COVID-19)",
               "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/technical-guidance","star"],
 
-                ["Italy","France24","Italy prolongs coronavirus lockdown measures to April 3",
-                "https://www.france24.com/en/20200319-italy-to-prolong-coronavirus-lockdown-measures"]
-              ];
+              ["Italy","France24","Italy prolongs coronavirus lockdown measures to April 3",
+                "https://www.france24.com/en/20200319-italy-to-prolong-coronavirus-lockdown-measures"],
+            ];
